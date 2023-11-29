91a53cf3eeef90b103064d33cfe990e75ecdb17f
arronkiptoo <kiptooaarron@gmail.com>
2023-11-29 22:16:56 +0300

    Completed Assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    def is_sentence(self):
+        if isinstance(self.value, str):
+            return self.value.endswith('.')
+        return False
+
+    def is_question(self):
+        if isinstance(self.value, str):
+            return self.value.endswith('?')
+        return False
+
+    def is_exclamation(self):
+        if isinstance(self.value, str):
+            return self.value.endswith('!')
+        return False
+
+    def count_sentences(self):
+        if isinstance(self.value, str):
+            # Updated logic to count sentences
+            sentences = [sentence.strip() for sentence in self.value.split('.') if sentence.strip()]
+            return len(sentences)
+        return 0
